cc2119e3c47b346de90df9bbfa396d17477a1386
AdmirDemaj1 <ademaj@unitsystem.net>
2022-07-08 10:31:39 +0000

    day-5-updated-view-created-new-endpoints

--- app-form/src/components/Question/Question.js
+++ app-form/src/components/Question/Question.js
@@ -1,84 +1,86 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "../../services/hasura-client";
-import classes from './Question.module.css';
-import Typography from '@mui/material/Typography';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import Button from '@mui/material/Button';
-import { useNavigate  } from "react-router-dom";
-import axios from 'axios';
-
-
-
-
+import classes from "./Question.module.css";
+import Typography from "@mui/material/Typography";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Question = (props) => {
-    const [questions, setQuestions] = useState({});
-    const [answers, setAnswers] = useState({});
-    const [value, setValue] = React.useState(true);
+  const [questions, setQuestions] = useState({});
+  const [answers, setAnswers] = useState({});
+  const [value, setValue] = React.useState(true);
 
-  
-    const history = useNavigate();
+  const history = useNavigate();
 
-    useEffect(() => {
-        axios.get('https://8080-egikarakash-skillmatrix-486iduh3y8n.ws-eu51.gitpod.io/api/rest/answers')
-        .then(res => {
-            //console.log("res-data", res.data.Answers[0].data);
-            setAnswers(res.data.Answers[0].data);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(
+        "https://8080-egikarakash-skillmatrix-486iduh3y8n.ws-eu51.gitpod.io/api/rest/answers"
+      )
+      .then((res) => {
+        //console.log("res-data", res.data.Answers[0].data);
+        setAnswers(res.data.Answers[0].data);
+      });
+  }, []);
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://8080-egikarakash-skillmatrix-486iduh3y8n.ws-eu51.gitpod.io/api/rest/get-question"
+      )
+      .then((res) => {
+        console.log("res-dataQ", res.data.Questions[0].data);
+        setQuestions(res.data.Questions[0].data);
+      });
+  }, []);
 
+  const handleClick = () => {
+    history("/");
+  };
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    useEffect(() => {
-        axios.get('https://8080-egikarakash-skillmatrix-486iduh3y8n.ws-eu51.gitpod.io/api/rest/get-question')
-        .then(res => {
-            console.log("res-dataQ", res.data.Questions[0].data);
-            setQuestions(res.data.Questions[0].data);
-        });
-    }, []);
-
-
-    const handleClick = () => {
-        history('/');
-    }
-            
-
-    const handleChange = (event) => {
-        setValue(event.target.value);
-      };
-
-        
-
-    return (
-        <div className={classes.question}>
-           <Typography variant="h6" component="div" gutterBottom>
-                {questions.Question}
-            </Typography>
-            <FormControl>
-                <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue="female"
-                    name="radio-buttons-group"
-                    value={value}
-                    onChange={handleChange}
-                >
-                    {Object.keys(answers).map(function(key, index) {
-                        // console.log("key:", key, "-", Object.values(answers)[index]);
-                        // console.log("index: ",  (index + 1));
-                        return <FormControlLabel key={key} value={Object.values(answers)[index]} control={<Radio />} label={key} />
-                    })}
-
-                </RadioGroup>
-            </FormControl>
-            <div style={{marginTop: "20px"}}>
-                <Button onClick={handleClick} variant="contained">Next Question</Button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={classes.question}>
+      <Typography variant="h6" component="div" gutterBottom>
+        {questions.Question}
+      </Typography>
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          {Object.keys(answers).map(function (key, index) {
+            // console.log("key:", key, "-", Object.values(answers)[index]);
+            // console.log("index: ",  (index + 1));
+            return (
+              <FormControlLabel
+                key={key}
+                value={Object.values(answers)[index]}
+                control={<Radio />}
+                label={key}
+              />
+            );
+          })}
+        </RadioGroup>
+      </FormControl>
+      <div style={{ marginTop: "20px" }}>
+        <Button onClick={handleClick} variant="contained">
+          Next Question
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Question;
--- app-form/src/components/WelcomePage/WelcomePage.js
+++ app-form/src/components/WelcomePage/WelcomePage.js
@@ -1,38 +1,66 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import classes from './WelcomePage.module.css';
-import { useNavigate  } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import classes from "./WelcomePage.module.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+// import { getDefaultState } from "react-query/types/core/mutation";
 
 const WelcomePage = (props) => {
-    const history = useNavigate();
+  const [openUntil, setOpenUntil] = useState("");
 
+  const history = useNavigate();
 
-    const handleClick = () => {
-        history('/fill');
-    }
+  const handleClick = () => {
+    history("/fill");
+  };
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://8080-egikarakash-skillmatrix-486iduh3y8n.ws-eu51.gitpod.io/api/rest/survey-openuntil"
+      )
+      .then((res) => {
+        console.log("res-data", res.data.Survey[0]["open_until"]);
+        setOpenUntil(res.data.Survey[0]["open_until"]);
+      });
+  }, []);
+  console.log(openUntil);
+  //console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(openUntil));
 
-    return (
-        <div className={classes.welcome}>
-            <Typography variant="h3" component="div" gutterBottom>
-                Welcome to the skills survey!
-            </Typography>
-            <Typography variant="string" paragraph component="div" gutterBottom>
-                This is a simple app that allows you to fill out a survey about your skills.
-            </Typography>
-            <Typography variant="string" paragraph component="div" gutterBottom>
-                You can interrupt the survey at any time and pick up later within 7 days.
-            </Typography>
-            <Typography variant="string" paragraph component="div" gutterBottom>
-                To get started, click the button below.
-            </Typography>
-            <div className={classes.btn}>
-                <Button onClick={handleClick} variant="contained">Get Started</Button>
-            </div>
-        </div>
-    );
-}
+  const date = new Date(openUntil);
+
+  return (
+    <div className={classes.welcome}>
+      <Typography variant="h3" component="div" gutterBottom>
+        Welcome to the skills survey!
+      </Typography>
+      <Typography variant="string" paragraph component="div" gutterBottom>
+        This is a simple app that allows you to fill out a survey about your
+        skills.
+      </Typography>
+      <Typography variant="string" paragraph component="div" gutterBottom>
+        You can interrupt the survey at any time and pick up later within 7
+        days.
+      </Typography>
+      {/* <Typography variant="string" paragraph component="div" gutterBottom>
+                {openUntil}
+            </Typography> */}
+      {"You have access to the survey until: " +
+        date.getDate() +
+        "th of " +
+        date.toLocaleString("default", { month: "long" }) + "  " +
+        date.toLocaleTimeString("default", { hour: "numeric", minute: "numeric"}) + "."}
+      <Typography variant="string" paragraph component="div" gutterBottom>
+        To get started, click the button below.
+      </Typography>
+      <div className={classes.btn}>
+        <Button onClick={handleClick} variant="contained">
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default WelcomePage;
